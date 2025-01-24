fa6705a1e9de9dddfa24ea9989f8451614be6c9d
Himanshu Verma <himanshuverma544@gmail.com>
2025-01-24 12:08:30 +0530

    add: added useClient hook

---
+++ hooks/useClient.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react";
+
+
+const useClient = () => {
+
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  return isClient;
+}
+
+
+export default useClient;
